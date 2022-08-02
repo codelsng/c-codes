c08b26944c5ccd5e186f55b2f94bf92c2558c2ae
codelsng <luofengzhiyuan@gmail.com>
2022-08-02 22:49:02 +0800

    test_8_2_2022

---
+++ Test/test_8_2_2022/test_8_2_2022/test.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+//第一题：下列代码执行结果是什么？
+//int main(void)
+//{
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		if (i = 5)
+//			printf("%d", i);
+//	}
+//
+//	return 0;
+//}
+
+//func(1)的值是多少？
+//int func(int n)
+//{
+//	int b;
+//	switch (n)
+//	{
+//	case 1: b = 30;
+//	case 2: b = 20;
+//	case 3: b = 10;
+//	default: b = 0;
+//	}
+//	return b;
+//}
+//
+//int main(void)
+//{
+//	printf("%d\n", func(1));
+//
+//	return 0;
+//}
