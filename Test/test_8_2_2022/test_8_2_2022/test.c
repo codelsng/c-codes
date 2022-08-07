a17a2c70b92d1dde3781abf182ab03b26201e127
codelsng <luofengzhiyuan@gmail.com>
2022-08-07 21:29:58 +0800

    test_8_2_2022/

--- Test/test_8_2_2022/test_8_2_2022/test.c
+++ Test/test_8_2_2022/test_8_2_2022/test.c
@@ -3,17 +3,18 @@
 #include <string.h>
 
 //第一题：下列代码执行结果是什么？
-//int main(void)
-//{
-//	int i = 0;
-//	for (i = 0; i < 10; i++)
-//	{
-//		if (i = 5)
-//			printf("%d", i);
-//	}
-//
-//	return 0;
-//}
+//死循环输出5
+int main(void)
+{
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		if (i = 5)
+			printf("%d", i);
+	}
+
+	return 0;
+}
 
 //func(1)的值是多少？
 //int func(int n)
