8c331cf5d636f7b8487b068a2c0245b7439e73a1
codelsng <luofengzhiyuan@gmail.com>
2022-08-11 18:29:17 +0800

    test_8_10_2022

---
+++ Test/test_8_10_2022/test_8_10_2022/test.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+////交换两数值的函数：
+//void Swap(int* num1, int* num2) {
+//	int temp = *num1;
+//	*num1 = *num2;
+//	*num2 = temp;
+//}
+//
+//int main(void)
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//
+//	printf("交换前的num1 = %d, num2 = %d\n", num1, num2);
+//	Swap(&num1, &num2);
+//	printf("交换后的num1 = %d, num2 = %d\n", num1, num2);
+//
+//	return 0;
+//}
+
+
+
+//判断100~200之间的素数：
+
+//判断1000~2000年的闰年：
+
+
+//二分查找法：
+
+int main(void)
+{
+
+
+	return 0;
+}
