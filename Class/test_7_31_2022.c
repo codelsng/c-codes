7015ee4774cf70ff879b2a0a46ac1c831075fc0e
codelsng <luofengzhiyuan@gmail.com>
2022-08-19 13:06:24 +0800

    Class

---
+++ Class/test_7_31_2022.c
@@ -0,0 +1,58 @@
+//作业：
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+
+
+//第一题：下列程序输出什么？
+//int main(void)
+//{
+//	char arr[] = { 'a', 'b', 'c' };
+//	printf("%d\n", strlen(arr));
+//	return 0;
+//}
+
+
+
+//第二题：下列程序输出的结果是什么？
+//int main(void)
+//{
+//	printf("%zd\n", strlen("c:\test\121"));
+//	printf("%zd\n", sizeof("c:\test\121"));
+//
+//	return 0;
+//}
+
+
+
+//第三题：写一个函数输出两个整数中的较大值：
+//void max(int num1, int num2);
+int max(int num1, int num2);
+int main(void)
+{
+	int num1 = 0;
+	int num2 = 0;
+	puts("Enter two intgers in this command line please.");
+	scanf("%d%d", &num1, &num2);
+	printf("max is %d\n", max(num1, num2));
+
+	//Warning：之前写成下面这样了………………>_<………………
+	//void max(num1, num2);
+
+	return 0;
+}
+int max(int num1, int num2)
+{
+	if (num1 < num2)
+		num1 = num2;
+	return num1;
+}
+
+//不足的函数：不够通用，最好只求值，打印可能为多余内容
+//void max(int num1, int num2)
+//{
+//	if (num1 < num2)
+//		num1 = num2;
+//	printf("max is %d\n", num1);
+//}
