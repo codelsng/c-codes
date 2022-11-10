882ddad76ea9bc025ae81a00d7cc50903be723e3
codelsng <luofengzhiyuan@gmail.com>
2022-11-10 22:44:08 +0800

    Chess

---
+++ Small projects/Chess/main.c
@@ -0,0 +1,28 @@
+﻿#include "game.h"
+
+int main(void)
+{
+	int input = 0;
+	srand(time(NULL));
+	do
+	{
+		menu();
+		scanf("%d", &input);
+		if (input == 1)
+		{
+			game();
+		}
+		else if (input == 0)
+		{
+			puts("Bye!");
+			break;
+		}
+		else
+		{
+			puts("Enter is error, re-enter please.");
+			continue;
+		}
+	} while (input);
+
+	return 0;
+}
